c2dbadaed1d1b42f38d2d58964b390bd0c2eb6fd
sully-pro <delemos.sullivan@gmail.com>
2024-09-09 16:26:06 +0200

    Note - Implementation of Note Controller

---
+++ frontend/src/main/java/com/medilabo/frontend/controller/PatientNotesController.java
@@ -0,0 +1,46 @@
+package com.medilabo.frontend.controller;
+
+import com.medilabo.note.model.Note;
+import com.medilabo.note.service.NoteService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+@Log4j2
+public class PatientNotesController {
+
+    @Autowired
+    private NoteService noteService;
+
+    @GetMapping("/patients/{patientId}/notes")
+    public String getPatientNotes(@PathVariable String patientId, Model model) {
+        log.info("Received request to get patient notes");
+
+        Optional<List<Note>> notes = noteService.getNotesByPatientId(patientId);
+
+        if (notes.isPresent()) {
+            model.addAttribute("notes", notes.get());
+            return "notes";
+        } else {
+            model.addAttribute("error", "No notes found for patient ID: " + patientId);
+            return "error";
+        }
+    }
+
+    @PostMapping("/patients/{patientId}/notes")
+    public String addNote(@PathVariable String patientId, Note note) {
+        log.info("Received request to add note: {}", note);
+        noteService.addNote(note);
+        return "redirect:/patients/" + patientId + "/notes";
+    }
+
+}
--- note/src/main/java/com/medilabo/note/service/NoteService.java
+++ note/src/main/java/com/medilabo/note/service/NoteService.java
@@ -1,10 +1,12 @@
 package com.medilabo.note.service;
 
 import com.medilabo.note.model.Note;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface NoteService {
 
     Optional<List<Note>> getNotesByPatientId(String patientId);
