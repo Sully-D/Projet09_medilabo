441b465303b6cf0368a0adf0039feefffa5c29c8
sully-pro <delemos.sullivan@gmail.com>
2024-09-23 17:46:05 +0200

    Add microservice risklevel

---
+++ risklevel/src/main/java/com/medilabo/risklevel/RisklevelApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.risklevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisklevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisklevelApplication.class, args);
+	}
+
+}
---
+++ risklevel/src/test/java/com/medilabo/risklevel/RisklevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.risklevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisklevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
