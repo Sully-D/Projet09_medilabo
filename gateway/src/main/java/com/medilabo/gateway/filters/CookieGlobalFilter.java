75b86f9f990b520508d9588c23c12145f9509f63
sully-pro <delemos.sullivan@gmail.com>
2024-10-08 09:47:03 +0200

    Add JavaDocs

--- gateway/src/main/java/com/medilabo/gateway/filters/CookieGlobalFilter.java
+++ gateway/src/main/java/com/medilabo/gateway/filters/CookieGlobalFilter.java
@@ -8,22 +8,34 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+/**
+ * Global filter to add and forward the JSESSIONID cookie to all backend services.
+ * This allows the backend services to share the same session.
+ */
 @Component
 public class CookieGlobalFilter implements GlobalFilter {
 
+    /**
+     * Adds the JSESSIONID cookie to all incoming requests and forwards it to all backend services.
+     * This allows the backend services to share the same session.
+     * @param exchange the current server web exchange
+     * @param chain the gateway filter chain
+     * @return a Mono that filters the web exchange
+     */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // Récupérer le cookie JSESSIONID de la requête
+
+        // get cookie
         HttpCookie sessionCookie = exchange.getRequest().getCookies().getFirst("JSESSIONID");
 
         if (sessionCookie != null) {
             String sessionId = sessionCookie.getValue();
-            System.out.println("JSESSIONID trouvé : " + sessionId);
+            System.out.println("JSESSIONID found : " + sessionId);
 
-            // Ajouter le cookie dans la réponse
+            // add cookie to response
             exchange.getResponse().getHeaders().add("Set-Cookie", "JSESSIONID=" + sessionId);
 
-            // Propager le cookie dans la requête
+            // add cookie to request
             ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
                     .header("Cookie", "JSESSIONID=" + sessionId)
                     .build();
@@ -31,7 +43,7 @@ public class CookieGlobalFilter implements GlobalFilter {
             return chain.filter(exchange.mutate().request(modifiedRequest).build());
         }
 
-        // Si pas de cookie, continuer sans modification
+        // no cookie, do nothing
         return chain.filter(exchange);
     }
 }
