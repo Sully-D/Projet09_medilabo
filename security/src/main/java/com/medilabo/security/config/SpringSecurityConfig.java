4266c5d0cf034f8120b65dc1ac94f817aaa6d177
sully-pro <delemos.sullivan@gmail.com>
2024-10-08 10:30:01 +0200

    Add JavaDocs

--- security/src/main/java/com/medilabo/security/config/SpringSecurityConfig.java
+++ security/src/main/java/com/medilabo/security/config/SpringSecurityConfig.java
@@ -18,10 +18,26 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 
 import java.io.IOException;
 
+
+/**
+ * Configuration class for Spring Security settings.
+ * Defines security configurations for the application, including custom login page, authentication success handler,
+ * user details service, and password encoder.
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    /**
+     * Defines the security filter chain configuration for HTTP requests.
+     * Disables CSRF protection, permits access to specific request matchers like '/auth/**', '/login', and '/error',
+     * requires authentication for any other request, sets up a custom login page at '/auth/login',
+     * specifies the URL for submitting the login form, and uses a custom authentication success handler.
+     *
+     * @param http the HttpSecurity object to configure the security filter chain
+     * @return the configured SecurityFilterChain object
+     * @throws Exception if an error occurs during configuration
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -31,25 +47,34 @@ public class SpringSecurityConfig {
                     auth.anyRequest().authenticated();
                 })
                 .formLogin(form -> form
-                        .loginPage("/auth/login")  // Page de connexion personnalisée
-                        .loginProcessingUrl("/login")  // URL utilisée pour soumettre le formulaire de login
-                        .successHandler(authenticationSuccessHandler())  // Utiliser le handler personnalisé
+                        .loginPage("/auth/login") // Login page
+                        .loginProcessingUrl("/login")  // URL used to submit the login form
+                        .successHandler(authenticationSuccessHandler())  // Using the custom handler
                         .permitAll()
                 )
                 .build();
     }
 
+    /**
+     * Defines a custom authentication success handler for redirecting users to the gateway URL upon successful authentication.
+     *
+     * @return an AuthenticationSuccessHandler object that redirects to 'http://192.168.1.87:8080/patients'
+     */
     @Bean
     public AuthenticationSuccessHandler authenticationSuccessHandler() {
         return new SimpleUrlAuthenticationSuccessHandler() {
             @Override
             public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-                // Redirection vers l'URL du gateway
-                response.sendRedirect("http://192.168.1.87:8080/patients");
+                // Redirect to the gateway URL
+                response.sendRedirect("http://localhost:8080/patients");
             }
         };
     }
 
+    /**
+     * Creates a UserDetails object for a user with username 'user', encoded password 'user', and role 'USER'.
+     * Returns an InMemoryUserDetailsManager containing the created user details.
+     */
     @Bean
     public UserDetailsService users() {
         UserDetails user = User.builder()
@@ -59,6 +84,9 @@ public class SpringSecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
+    /**
+     * Returns a new BCryptPasswordEncoder instance.
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- security/src/main/java/com/medilabo/security/controller/LoginController.java
+++ security/src/main/java/com/medilabo/security/controller/LoginController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    /**
+     * Retrieves the login page.
+     *
+     * @return The name of the login page.
+     */
     @GetMapping("/auth/login")
     public String loginPage() {
         return "login";
